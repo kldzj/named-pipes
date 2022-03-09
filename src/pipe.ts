28b5dbca562ac92a87cb99b18751068d0b059eda
kldzj <github@kldzj.dev>
2022-03-09 17:35:39 +0100

    switch to private fields in NamedPipe

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,50 +1,49 @@
-import { existsSync, unlinkSync, accessSync, constants as FSC } from 'fs';
+import { existsSync, unlinkSync } from 'fs';
 import { getDebugLogger, Receiver, ReceiverOptions, Sender, SenderOptions } from '.';
 
 export class NamedPipe {
-  #path: string;
-
-  #sender?: Sender;
-  #receivers: Receiver[] = [];
-  #debug = getDebugLogger('pipe');
+  private _path: string;
+  private sender?: Sender;
+  private receivers: Receiver[] = [];
+  private debug = getDebugLogger('pipe');
 
   get path() {
-    return this.#path;
+    return this._path;
   }
 
   constructor(path: string) {
-    this.#path = path;
+    this._path = path;
   }
 
   public exists() {
-    return existsSync(this.#path);
+    return existsSync(this.path);
   }
 
   public createReceiver(opts?: ReceiverOptions) {
-    this.#debug('Creating receiver');
+    this.debug('Creating receiver');
     const receiver = new Receiver(this, opts);
-    this.#receivers.push(receiver);
+    this.receivers.push(receiver);
     return receiver;
   }
 
   public createSender(opts?: SenderOptions) {
-    if (this.#sender) {
-      return this.#sender;
+    if (this.sender) {
+      return this.sender;
     }
 
-    this.#debug('Creating sender');
-    this.#sender = new Sender(this, opts);
-    return this.#sender;
+    this.debug('Creating sender');
+    this.sender = new Sender(this, opts);
+    return this.sender;
   }
 
   public destroy() {
-    this.#debug('Destroying pipe');
-    this.#sender?.destroy();
-    this.#receivers.forEach((receiver) => receiver.destroy());
+    this.debug('Destroying pipe');
+    this.sender?.destroy();
+    this.receivers.forEach((receiver) => receiver.destroy());
 
     if (process.platform !== 'win32' && this.exists()) {
-      this.#debug('Unlinking pipe');
-      unlinkSync(this.#path);
+      this.debug('Unlinking pipe');
+      unlinkSync(this.path);
     }
   }
 }
